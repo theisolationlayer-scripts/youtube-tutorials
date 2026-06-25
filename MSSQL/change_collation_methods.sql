f3786dd1749f2b5568d905b0c3caeaa29a73c64c
theisolationlayer-scripts <theisolationlayer@gmail.com>
2026-06-25 10:32:23 -0400

    Create change_collation_methods.sql

---
+++ MSSQL/change_collation_methods.sql
@@ -0,0 +1,51 @@
+/*******************************************************************************
+  THE ISOLATION LAYER - www.youtube.com/@theisolationlayer
+  Changing instance collation on SQL Server
+
+  IMPORTANT: Always test scripts in a development/staging environment before 
+  running in production. The author is not responsible for any impact, data 
+  loss, or downtime resulting from the use of this script.
+*******************************************************************************/
+
+--Method #1 - supported by Microsoft
+  sqlcmd -S win1 -U sa -P Password1234 -C
+  select convert(varchar(256), SERVERPROPERTY('collation'));
+  GO
+  select name from sys.databases;
+  GO
+  exit
+  
+  net stop "SQL Server (MSSQLSERVER)"
+  
+  C:\Program Files\Microsoft SQL Server\170\Setup Bootstrap\SQL2025> .\setup.exe /QUIET /ACTION=REBUILDDATABASE /INSTANCENAME=MSSQLServer /SQLSYSADMINACCOUNTS=“win1\administrator” /SQLCOLLATION=Latin1_General_CS_AS /SAPWD=Password1234
+  
+  net start "SQL Server (MSSQLSERVER)"
+    
+  sqlcmd -S win1 -U sa -P Password1234 -C
+  select convert(varchar(256), SERVERPROPERTY('collation'));
+  Go
+  select name from sys.databases;
+  go
+
+--Method #2 - not supported by Microsoft
+  sqlcmd -S win1 -U sa -P Password1234 -C
+  select convert(varchar(256), SERVERPROPERTY('collation'));
+  Go
+  select name from sys.databases;
+  go
+  exit
+  
+  net stop "SQL Server (MSSQLSERVER)"
+  
+  “C:\Program Files\Microsoft SQL Server\MSSQL17.MSSQLSERVER\MSSQL\Binn\sqlservr.exe" -m -T4022 -T3659 =s”MSSQLServer” -q”SQL_Latin1_General_CP1_CI_AS”
+  
+  --Control - C and then start the instance and check it all again:
+  
+  net start "SQL Server (MSSQLSERVER)"
+  
+  sqlcmd -S win1 -U sa -P Password1234 -C
+  select convert(varchar(256), SERVERPROPERTY('collation'));
+  GO
+  select name from sys.databases;
+  GO
+  exit
