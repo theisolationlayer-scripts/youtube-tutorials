ea290b4a06e19c8b78d3c510093038f04f5e9c3f
theisolationlayer-scripts <theisolationlayer@gmail.com>
2026-06-25 10:44:10 -0400

    Update change_collation_methods.sql

--- MSSQL/change_collation_methods.sql
+++ MSSQL/change_collation_methods.sql
@@ -8,44 +8,44 @@
 *******************************************************************************/
 
 --Method #1 - supported by Microsoft
-  sqlcmd -S win1 -U sa -P Password1234 -C
-  select convert(varchar(256), SERVERPROPERTY('collation'));
-  GO
-  select name from sys.databases;
-  GO
-  exit
+sqlcmd -S win1 -U sa -P Password1234 -C
+select convert(varchar(256), SERVERPROPERTY('collation'));
+GO
+select name from sys.databases;
+GO
+exit
   
-  net stop "SQL Server (MSSQLSERVER)"
+net stop "SQL Server (MSSQLSERVER)"
   
-  C:\Program Files\Microsoft SQL Server\170\Setup Bootstrap\SQL2025> .\setup.exe /QUIET /ACTION=REBUILDDATABASE /INSTANCENAME=MSSQLServer /SQLSYSADMINACCOUNTS=“win1\administrator” /SQLCOLLATION=Latin1_General_CS_AS /SAPWD=Password1234
-  
-  net start "SQL Server (MSSQLSERVER)"
+C:\Program Files\Microsoft SQL Server\170\Setup Bootstrap\SQL2025> .\setup.exe /QUIET /ACTION=REBUILDDATABASE /INSTANCENAME=MSSQLServer /SQLSYSADMINACCOUNTS=“win1\administrator” /SQLCOLLATION=Latin1_General_CS_AS /SAPWD=Password1234
+
+net start "SQL Server (MSSQLSERVER)"
     
-  sqlcmd -S win1 -U sa -P Password1234 -C
-  select convert(varchar(256), SERVERPROPERTY('collation'));
-  Go
-  select name from sys.databases;
-  go
+sqlcmd -S win1 -U sa -P Password1234 -C
+select convert(varchar(256), SERVERPROPERTY('collation'));
+GO
+select name from sys.databases;
+GO
 
 --Method #2 - not supported by Microsoft
-  sqlcmd -S win1 -U sa -P Password1234 -C
-  select convert(varchar(256), SERVERPROPERTY('collation'));
-  Go
-  select name from sys.databases;
-  go
-  exit
+sqlcmd -S win1 -U sa -P Password1234 -C
+select convert(varchar(256), SERVERPROPERTY('collation'));
+GO
+select name from sys.databases;
+GO
+exit
   
-  net stop "SQL Server (MSSQLSERVER)"
+net stop "SQL Server (MSSQLSERVER)"
   
-  “C:\Program Files\Microsoft SQL Server\MSSQL17.MSSQLSERVER\MSSQL\Binn\sqlservr.exe" -m -T4022 -T3659 =s”MSSQLServer” -q”SQL_Latin1_General_CP1_CI_AS”
+“C:\Program Files\Microsoft SQL Server\MSSQL17.MSSQLSERVER\MSSQL\Binn\sqlservr.exe" -m -T4022 -T3659 =s”MSSQLServer” -q”SQL_Latin1_General_CP1_CI_AS”
   
-  --Control - C and then start the instance and check it all again:
+--Control - C and then start the instance and check it all again:
   
-  net start "SQL Server (MSSQLSERVER)"
+net start "SQL Server (MSSQLSERVER)"
   
-  sqlcmd -S win1 -U sa -P Password1234 -C
-  select convert(varchar(256), SERVERPROPERTY('collation'));
-  GO
-  select name from sys.databases;
-  GO
-  exit
+sqlcmd -S win1 -U sa -P Password1234 -C
+select convert(varchar(256), SERVERPROPERTY('collation'));
+GO
+select name from sys.databases;
+GO
+exit
